15412c8d23060b8d3d44e3485c6adede7c1592a5
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-09-20 19:32:59 +0700

    feat: Add configuration file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-import { SSOAuthenticator } from './services/sso';
+import config from './../config.json';
+import { DateTime } from 'luxon';
 
-// const auth = new SSOAuthenticator();
+const { day, hour } = DateTime.local().setZone('Asia/Jakarta');
+
+if (config[day].includes(hour)) {
+  // do something :)
+}
