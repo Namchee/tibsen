9433c4941ded4520d1920849e5376f0a03f092d1
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-09-22 13:18:14 +0700

    feat: Add command-line as input method

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,31 @@
 import config from './../config.json';
 import { DateTime } from 'luxon';
+import { createHTTPClient } from './common/client';
+import { SSOAuthenticator } from './services/sso';
+import { StuPorCrawler } from './services/stupor';
 
-const { day, hour } = DateTime.local().setZone('Asia/Jakarta');
+(async () => {
+  const { day, hour } = DateTime.local().setZone('Asia/Jakarta');
 
-if (config[day].includes(hour)) {
-  // do something :)
-}
+  if (config[day].includes(hour)) {
+    const [username, password] = process.argv.slice(2);
+
+    if (!username) {
+      throw new Error('Username must be supplied.');
+    }
+
+    if (!password) {
+      throw new Error('Password must be supplied.');
+    }
+
+    const httpClient = createHTTPClient();
+    const authenticator = new SSOAuthenticator(httpClient, username, password);
+    const crawler = new StuPorCrawler(authenticator);
+
+    const result = await crawler.markPresence();
+
+    if (!result) {
+      throw new Error('Presence marking failed...');
+    }
+  }
+})();
