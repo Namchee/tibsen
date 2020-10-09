8eccb8bfdcfc724b1f157a92ba1293c8c2c5753f
Cristopher <32661241+Namchee@users.noreply.github.com>
2020-10-09 18:27:32 +0700

    refactor: Change service model (#1)
    refactor: Change service model

---
+++ src/common/request.ts
@@ -0,0 +1,39 @@
+import { ParameterError } from '../exception/parameter';
+
+export interface RequestBody {
+  email: string;
+  password: string;
+  schedule: number[][];
+}
+
+export function isRequestBody(body: any): asserts body is RequestBody {
+  const pattern = /(\D+)@student\.unpar\.ac\.id/;
+
+  if (!body.email) {
+    throw new ParameterError('Email must be provided');
+  }
+
+  const email = body.email as string;
+
+  if (email.match(pattern) !== null) {
+    throw new ParameterError('Invalid email');
+  }
+
+  if (!body.password) {
+    throw new ParameterError('Password must be provided');
+  }
+
+  if (!body.schedule) {
+    throw new ParameterError('Schedule must be provided');
+  }
+
+  if (!Array.isArray(body.schedule)) {
+    throw new ParameterError(
+      'Schedule must be an 2 dimensional array of numbers',
+    );
+  }
+
+  if (body.schedule.length !== 5) {
+    throw new ParameterError('Invalid schedule format');
+  }
+}
